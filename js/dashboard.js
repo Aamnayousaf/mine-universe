925ca9951072efc333740d9ddadb60d240efa849
Your amna <amnayousaf823@gmail.com>
2023-08-05 12:18:58 +0500

    project

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,36 +1,46 @@
-// // JavaScript to handle the button click and show all hidden divs
-// document.getElementById('viewAllButton').addEventListener('click', function() {
-//     var divs = document.getElementsByClassName('list')[0].children;
-//     for (var i = 0; i < divs.length; i++) {
-//       divs[i].classList.toggle('show');
-//     }
-//   });
-  
 
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var toggleButton = document.getElementById('toggleButton');
+    var sidebar = document.getElementById('sidebar');
+
+    toggleButton.addEventListener('click', function() {
+      // Check if the sidebar is currently hidden
+      var isSidebarHidden = window.getComputedStyle(sidebar).display === 'none';
+
+      // Toggle the sidebar's display based on its current state
+      if (isSidebarHidden) {
+        sidebar.style.display = 'block';
+      } else {
+        sidebar.style.display = 'none';
+      }
+    });
+  });
 // JavaScript to handle the button click and show/hide all hidden divs
 document.getElementById('viewAllButton').addEventListener('click', function() {
-    var divs = document.getElementsByClassName('list')[0].children;
-    var viewAllButton = document.getElementById('viewAllButton');
-  
-    for (var i = 0; i < divs.length; i++) {
-      if (divs[i].classList.contains('show')) {
-        divs[i].classList.remove('show');
-      } else {
-        divs[i].classList.add('show');
-      }
-    }
-  
-    // Toggle the button label
-    if (viewAllButton.textContent === 'Hide') {
-        viewAllButton.textContent = 'view All';
-    } else {
-        viewAllButton.textContent = 'Hide';
-    }
-  });
-  
+  var divs = document.getElementsByClassName('list')[0].children;
+  var viewAllButton = document.getElementById('viewAllButton');
 
-  // ##### toggle button #####
-  function toggleSidebar() {
-    var sidebar = document.querySelector('.sidebar');
-    sidebar.classList.toggle('sidebar-expanded');
+  for (var i = 0; i < divs.length; i++) {
+    if (divs[i].classList.contains('show')) {
+      divs[i].classList.remove('show');
+    } else {
+      divs[i].classList.add('show');
+    }
   }
+
+  // Toggle the button label
+  if (viewAllButton.textContent === 'Hide') {
+      viewAllButton.textContent = 'view All';
+  } else {
+      viewAllButton.textContent = 'Hide';
+  }
+});
+
+
+// ##### toggle button #####
+function toggleSidebar() {
+  var sidebar = document.querySelector('.sidebar');
+  sidebar.classList.toggle('sidebar-expanded');
+}
